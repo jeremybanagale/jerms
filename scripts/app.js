bcf1026b029dfafaf8ca327a9ff79072f1c88162
Jigno Alfred V. Venezuela <jignoalfred.venezuela@live.com>
2017-04-10 19:01:27 +0800

    Added chartCtrl to create progress bar

--- scripts/app.js
+++ scripts/app.js
@@ -18,3 +18,32 @@ angular.module("portfolioPage", ["ngMaterial"])
             ]
         };
     })
+
+    .controller('chartCtrl', function($scope) {
+        $scope.languages = [{
+            name: "PHP",
+            percent: "30%",
+            offset: '181'
+        },
+        {
+            name: "VB.NET",
+            percent: "75%",
+            offset: '481'
+        },
+        {
+            name: "Python",
+            percent: "45%",
+            offset: '281'
+        },
+        {
+            name: "Javascript",
+            percent: "80%",
+            offset: '521'
+        },
+        {
+            name: "HTML/CSS",
+            percent: "58%",
+            offset: '321'
+        },
+      ];
+    })
