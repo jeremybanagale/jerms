1aa9f065e5f615d795adcb114de20a94943f978b
Jigno Alfred V. Venezuela <jignoalfred.venezuela@live.com>
2017-04-24 18:07:36 +0800

    Added working toast for todoCtrl

--- scripts/app.js
+++ scripts/app.js
@@ -14,11 +14,6 @@ angular.module("portfolioPage", ["ngMaterial", "ngResource", "ngAnimate"])
 
   .controller('languageCtrl', function($scope) {
     $scope.languages = languages;
-
-    $scope.reload = function() {
-      // location.reload();
-      $state.reload();
-    }
   })
 
   .controller('wikiCtrl', function($scope, $resource) {
@@ -53,209 +48,103 @@ angular.module("portfolioPage", ["ngMaterial", "ngResource", "ngAnimate"])
     })
   })
 
-  .controller('locationCtrl', function($scope, $http, $rootScope) {
-    $scope.getLoc = function() {
-      $scope.myLoc = '';
-
-      //NOTE: jsonp is required for cross origin requests.
-      $http.jsonp("http://ip-api.com/json/?callback=JSON_CALLBACK")
-        .success(function(data) {
-          $scope.myLoc = data;
-          $rootScope.loc = {
-            'lat': $scope.myLoc.lat,
-            'lon': $scope.myLoc.lon
-          };
-
-          //Update weather now that we have location.
-          $rootScope.$emit('updateWeather');
-        })
-
-        .error(function(data) {
-          $scope.myLoc = "Request failed";
-        });
-    };
-
-    $scope.getLoc();
-  })
-
-  .controller('weatherCtrl', function($scope, $http, $rootScope) {
-    //Allow cross controller calls.
-    $rootScope.$on('updateWeather', function() {
-      $scope.getWeather();
-    });
-
-    $scope.getWeather = function() {
-      $scope.wForecast = '';
-      var city = $rootScope.loc;
-
-      $http.jsonp("http://api.openweathermap.org/data/2.5/weather?lat=" + city.lat + "&lon=" + city.lon + "&units=metric&appid=907c6fe2c953ace0643a570472baef1a&callback=JSON_CALLBACK", {
-          dataType: 'json'
-        })
-
-        .success(function(data) {
-          $scope.wForecast = {
-            'humidity': data.main.humidity,
-            'temp': data.main.temp,
-            'abs': data.weather[0]
-          };
-        })
-
-        .error(function(data) {
-          $scope.woeid = "Request failed";
-          console.log('data', data);
-        });
-    };
-  })
-
-  .controller('todoCtrl', function($scope, $rootScope, $timeout) {
-    var today = curDate();
-    //Load from local storage
-    loadList();
-
-    if (!$scope.toDoList) {
-      $scope.toDoList = [{
-        desc: 'Create portfolio...',
-        done: true,
-        due: '',
-        dt: today
-      }, {
-        desc: 'Send CV and portfolio!',
-        done: false,
-        due: 'Apr 30, 2017',
-        dt: today
-      }, {
-        desc: 'Pray daily...',
-        done: false,
-        due: '',
-        dt: today
+  .controller('taskCtrl', function($scope, $mdShowToast) {
+    $scope.today = new Date();
+    $scope.saved = localStorage.getItem('taskItems');
+    $scope.taskItem = (localStorage.getItem('taskItems') !== null) ?
+      JSON.parse($scope.saved) : [{
+        description: "Why not add a task?",
+        category: "Personal",
+        date: $scope.today,
+        complete: false
       }];
-    };
+    localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
 
-    $scope.addMode = false;
-    $scope.newItem = {
-      desc: '',
-      done: false,
-      due: '',
-      dt: today
-    };
+    $scope.newTask = null;
+    $scope.newTaskDate = null;
+    $scope.categories = [{
+        name: 'Personal'
+      },
+      {
+        name: 'Work'
+      },
+      {
+        name: 'School'
+      },
+      {
+        name: 'Business'
+      },
+      {
+        name: 'Other'
+      }
+    ];
 
-    $scope.addItem = function() {
-      $scope.toDoList.push($scope.newItem);
-      $scope.newItem = {
-        desc: '',
-        done: false,
-        due: '',
-        dt: curDate()
+    $scope.newTaskCategory = $scope.categories;
+    $scope.addNew = function() {
+      if ($scope.newTaskDate == null || $scope.newTaskDate == '') {
+        $scope.taskItem.push({
+          description: $scope.newTask,
+          date: "No deadline",
+          complete: false,
+          category: $scope.newTaskCategory.name
+        })
+      } else {
+        $scope.taskItem.push({
+          description: $scope.newTask,
+          date: $scope.newTaskDate,
+          complete: false,
+          category: $scope.newTaskCategory.name
+        })
       };
-
-      $scope.toggleForm();
-      saveList();
+      $scope.newTask = '';
+      $scope.newTaskDate = '';
+      $scope.newTaskCategory = $scope.categories;
+      localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
+      $mdShowToast.show('Save');
     };
 
-    $scope.toggleForm = function() {
-      $scope.addMode = !$scope.addMode;
-    };
-
-    $scope.clearDoneItems = function() {
-      var curList = $scope.toDoList;
-
-      $scope.toDoList = [];
-      angular.forEach(curList, function(item) {
-        if (!item.done) {
-          console.log('sss', item);
-          $scope.toDoList.push(item);
+    $scope.deleteTask = function() {
+      var completedTask = $scope.taskItem;
+      $scope.taskItem = [];
+      angular.forEach(completedTask, function(taskItem) {
+        if (!taskItem.complete) {
+          $scope.taskItem.push(taskItem);
         }
       });
-
-      //Note: Temp workaround. Manual delete from local storage if still fail.
-      document.querySelector('.is-checked').MaterialCheckbox.uncheck();
-      saveList();
+      localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
+      $mdShowToast.show('Delete');
     };
 
-    function saveList() {
-      localStorage.setItem('toDoList', JSON.stringify($scope.toDoList));
-      toastThis('Data saved on browser storage.')
-    }
-
-    function loadList() {
-      var x = localStorage.getItem('toDoList');
-
-      if (x != null) {
-        $scope.toDoList = JSON.parse(x);
-      }
-      toastThis('Data loaded from browser storage.');
-    }
-
-    function curDate() {
-      var n = Date.now();
-      return n;
+    $scope.save = function() {
+      localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
     };
-
-    //toast status messages
-    function toastThis(msg) {
-      $rootScope.toast = {
-        msg: msg,
-        show: true
-      };
-
-      $timeout(function() {
-        $rootScope.toast.show = false;
-      }, 2000);
-    }
   })
 
-  .controller('taskCtrl', function($scope) {
-      $scope.today = new Date();
-      $scope.saved = localStorage.getItem('taskItems');
-      $scope.taskItem = (localStorage.getItem('taskItems')!==null) ?
-      JSON.parse($scope.saved) : [ {description: "Why not add a task?", category: "Other", date: $scope.today, complete: false}];
-      localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
-
-      $scope.newTask = null;
-      $scope.newTaskDate = null;
-      $scope.categories = [
-          {name: 'Personal'},
-          {name: 'Work'},
-          {name: 'School'},
-          {name: 'Cleaning'},
-          {name: 'Other'}
-      ];
-      $scope.newTaskCategory = $scope.categories;
-      $scope.addNew = function () {
-          if ($scope.newTaskDate == null || $scope.newTaskDate == '') {
-              $scope.taskItem.push({
-                  description: $scope.newTask,
-                  date: "No deadline",
-                  complete: false,
-                  category: $scope.newTaskCategory.name
-              })
-          } else {
-              $scope.taskItem.push({
-                  description: $scope.newTask,
-                  date: $scope.newTaskDate,
-                  complete: false,
-                  category: $scope.newTaskCategory.name
-              })
-          };
-          $scope.newTask = '';
-          $scope.newTaskDate = '';
-          $scope.newTaskCategory = $scope.categories;
-          localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
-      };
-      $scope.deleteTask = function () {
-          var completedTask = $scope.taskItem;
-          $scope.taskItem = [];
-          angular.forEach(completedTask, function (taskItem) {
-              if (!taskItem.complete) {
-                  $scope.taskItem.push(taskItem);
-              }
-          });
-          localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
-      };
-
-      $scope.save = function () {
-          localStorage.setItem('taskItems', JSON.stringify($scope.taskItem));
+  .controller('themeCtrl', function($scope) {
+    $scope.theme = 'default';
+    $scope.changeTheme = function() {
+      var themes = ['default', 'indigo', 'lime', 'orange', 'cyan', 'pink', 'brown'];
+      var randomize = function(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
       }
+
+      $scope.theme = randomize(themes);
+    };
+  })
+
+  .service('$mdShowToast', function($mdToast) {
+    return {
+      show: function(content) {
+      return $mdToast.show(
+        $mdToast.simple()
+          .content(content)
+          .action('OK')
+          .highlightAction(true)
+          .highlightClass('md-accent')
+          .position('top right')
+          .hideDelay(6000)
+      )}
+    };
   })
 
   .config(function($mdThemingProvider) {
@@ -288,16 +177,4 @@ angular.module("portfolioPage", ["ngMaterial", "ngResource", "ngAnimate"])
       .accentPalette('grey')
 
     $mdThemingProvider.alwaysWatchTheme(true);
-  })
-
-  .controller('themeCtrl', function($scope) {
-    $scope.theme = 'default';
-    $scope.changeTheme = function() {
-      var themes = ['default', 'indigo', 'lime', 'orange', 'cyan', 'pink', 'brown'];
-      var randomize = function(arr) {
-        return arr[Math.floor(Math.random() * arr.length)];
-      }
-
-      $scope.theme = randomize(themes);
-    };
   })
